bfa029e483b3429054b3361faa36085c28e03b0b
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-03-16 17:00:56 +0000

    Add app SearchBar component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,20 @@
-import './App.css';
+import "./App.css";
+import SearchBar from "../SearchBar/SearchBar.js";
 
 function App() {
   return (
     <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-    {/* <!-- Add a SearchBar component --> */}
-    <div className="App-playlist">
-      {/* <!-- Add a SearchResults component --> */}
-      {/* <!-- Add a Playlist component --> */}
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          {/* <!-- Add a SearchResults component --> */}
+          {/* <!-- Add a Playlist component --> */}
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 }
 
