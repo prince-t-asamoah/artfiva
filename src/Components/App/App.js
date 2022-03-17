56dc73a07652affb38e79a4f0eecbaf8f9ba2dfe
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-03-17 09:16:01 +0000

    Refactor function component to class component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,29 @@
-import './App.css';
-import {SearchBar} from '../SearchBar/SearchBar.js';
-import {SearchResults} from '../SearchResults/SearchResults.js';
-import {PlayList} from '../PlayList/PlayList.js';
+import React from "react";
+import "./App.css";
+import { SearchBar } from "../SearchBar/SearchBar.js";
+import { SearchResults } from "../SearchResults/SearchResults.js";
+import { PlayList } from "../PlayList/PlayList.js";
 
-function App() {
-  return (
-    <div>
-      <h1>
-        Ja<span className="highlight">mmm</span>ing
-      </h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults/>
-          <PlayList/>
+// class App extends React.Component {
+
+// }
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <PlayList />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
