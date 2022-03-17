f475a04257f1eb831354cfc8e5303083922c6c58
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-03-17 21:23:36 +0000

    Add map method to render tracks info

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./TrackList.css";
+import { Track } from "../Track/Track.js";
 
 export class TrackList extends React.Component {
+  
   render() {
+  const tracks = this.props.tracks.map(track => {
+    return <Track track={track} key={track.id}/>
+  })
     return (
       <div className="TrackList">
-        {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        {tracks}
       </div>
     );
   }
