96a30dd085b6be622ffd348f28ab2d33909368ea
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-03-17 06:03:13 +0000

    Change input attribute and value

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -6,7 +6,7 @@ export class PlayList extends React.Component {
   render() {
     return (
       <div className="Playlist">
-      <input value="New Playlist" />
+      <input defaultValue={"New Playlist"} />
       <TrackList/>
       <button className="Playlist-save">SAVE TO SPOTIFY</button>
     </div>
