785beef692b26b5e57186ad3b35d5ea9de9b53ee
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-03-17 05:46:20 +0000

    Refactor component main code

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,7 +1,7 @@
-import React from "react";
-import "./SearchBar.css";
+import React from 'react';
+import './SearchBar.css';
 
-class SearchBar extends React.Component {
+export class SearchBar extends React.Component {
   render() {
     return (
       <div className="SearchBar">
@@ -12,4 +12,3 @@ class SearchBar extends React.Component {
   }
 }
 
-export default SearchBar;
