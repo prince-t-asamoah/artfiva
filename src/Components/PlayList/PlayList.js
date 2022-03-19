8864c382122c9ece1c5ea24a98dbdb2cc7dd2f3e
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-03-19 13:11:33 +0000

    Add and bind handleNameChange method

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -3,10 +3,20 @@ import './PlayList.css';
 import { TrackList } from '../TrackList/TrackList';
 
 export class PlayList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    let nameValue = e.target.value;
+    this.props.onNameChange(nameValue);
+  }
+
   render() {
     return (
       <div className="Playlist">
-      <input defaultValue={"New Playlist"} />
+      <input name="playlist-name" defaultValue="New Playlist" />
       <TrackList
         tracks={this.props.playListTracks}
         isRemoval={true}
