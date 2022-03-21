48c66c2d32fabdc2b6388f21ead3e7fd0374c41d
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-03-21 07:01:03 +0000

    Add and bind searchTrack method to this

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './SearchBar.css';
 
 export class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.searchTrack = this.searchTrack.bind(this);
+  }
+
+  searchTrack(e) {
+    this.props.onSearch(e.target.value);
+  }
+
   render() {
     return (
       <div className="SearchBar">
