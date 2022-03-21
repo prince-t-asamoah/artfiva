e99ee453896dbf0d4555f53728f00725c1dcc361
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-03-21 20:52:18 +0000

    Refactor module variables to object properties

--- src/Utility/Spotify.js
+++ src/Utility/Spotify.js
@@ -1,19 +1,28 @@
-let accessToken, tokenExpiresIn;
-const CLIENT_ID = '5a3f2e12dce84254b9e74f21ed388014';
-const REDIRECT_URI = 'http://localhost:3000/';
+// let accessToken, tokenExpiresIn;
+// const CLIENT_ID = '5a3f2e12dce84254b9e74f21ed388014';
+// const REDIRECT_URI = 'http://localhost:3000/';
 
 const spotify = {
+    accessToken: null,
+    tokenExpiresIn: null,
+    CLIENT_ID: '5a3f2e12dce84254b9e74f21ed388014',
+    REDIRECT_URI: 'http://localhost:3000/',
     getAccessToken() {
-        if (accessToken) {
-            return accessToken;
+        if (this.accessToken) {
+            return this.accessToken;
         } else {
             //Gets access token and its expiration time from URL
-            accessToken = window.location.href.match(/access_token=([^&]*)/);
-            tokenExpiresIn = window.location.href.match(/expires_in=([^&]*)/);
-            if (accessToken && tokenExpiresIn) {
-                window.setTimeout(() => accessToken = '', tokenExpiresIn * 1000);
+            let accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+            let tokenExpiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+            if (accessTokenMatch && tokenExpiresInMatch) {
+                this.accessToken = accessTokenMatch[1];
+                this.tokenExpiresIn = Number(tokenExpiresInMatch[1]);
+                //Clears accessToken and get new one when it expires
+                window.setTimeout(() => this.accessToken = '', this.tokenExpiresIn * 1000);
                 window.history.pushState('Access Token', null, '/');
-                window.history = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
+                return this.accessToken;
+            } else {
+                window.history = `https://accounts.spotify.com/authorize?client_id=${this.CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${this.REDIRECT_URI}`;
             }
         }
     }
