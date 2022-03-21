65aaca34d20ee192eed9d87a074778d98acac367
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-03-21 15:12:07 +0000

    Set timer to clear accessToken after token expiration

--- src/Utility/Spotify.js
+++ src/Utility/Spotify.js
@@ -5,8 +5,13 @@ const spotify = {
         if (accessToken) {
             return accessToken;
         } else {
+            //Gets access token and its expiration time from URL
             accessToken = window.location.href.match(/access_token=([^&]*)/);
             tokenExpiresIn = window.location.href.match(/expires_in=([^&]*)/);
+            if (accessToken && tokenExpiresIn) {
+                window.setTimeout(() => accessToken = '', tokenExpiresIn * 1000);
+                window.history.pushState('Access Token', null, '/');
+            }
         }
     }
 }
