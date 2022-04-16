55877b34cb4c6779903225546f700a8f317fa594
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-04-16 13:30:55 +0000

    Add savePlayList method to spotify utility

--- src/Utility/Spotify.js
+++ src/Utility/Spotify.js
@@ -48,6 +48,30 @@ const Spotify = {
                 }));
             }
         }).catch (error => console.log(error));
+    },
+    savePlayList(name, trackUris) {
+        if (!name || !trackUris) return;
+        const accessToken = this.getAccessToken();
+        const headers = { Authorization: `Bearer ${accessToken}`};
+        let userId;
+        return fetch(`https://api.spotify.com/v1/me`, { headers: headers})
+        .then(response => response.json())
+        .then(responseObj => {
+            userId = responseObj.id;
+            return fetch(`https:/api.spotify.com/v1/users/${userId}/playlists`, {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({name: name})
+            }).then(response => response.json())
+            .then(responseObj => {
+                const playlistId = responseObj.id;
+                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+                    headers: headers,
+                    method: 'POST',
+                    body: JSON.stringify({uris: trackUris})
+                })
+            })
+        })
     }
 }
 
